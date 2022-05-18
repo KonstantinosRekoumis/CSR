000c8ceae61810b7699ef8e929e9e60335fbc79e
KonstantinosRekoumis <62998716+KonstantinosRekoumis@users.noreply.github.com>
2022-05-18 17:21:51 +0300

    kaname ligh proodo

--- modules/rules.py
+++ modules/rules.py
@@ -3,7 +3,17 @@
 # #############################
 
 #Materials Constant Array see. CSR ... to be filled
+from modules.classes import plate
+
+
 MATERIALS = {
-    "A":235,
-    "AH":300
+    "A" : 235,
+    "AH32" : 315,
+    'AH36' : 355,
+    'AH40' : 390
+
 }
+# {'Reh' : 235, 'Rm' : 400}
+def scantling_req(st_plate : stif_plate, ship : ship) :
+
+def net_scantling(plate : plate,  ):
