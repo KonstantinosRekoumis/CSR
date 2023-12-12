93fae58160861cbe255c5eb8845995081d6455e9
gerelef <44060598+gerelef@users.noreply.github.com>
2023-12-12 23:38:24 +0200

    added automatic __str__ implementation decorator

--- modules/utilities.py
+++ modules/utilities.py
@@ -94,6 +94,21 @@ class Logger:
         raise BaseException(*args)
 
 
+# Writing boilerplate code to avoid writing boilerplate code!
+# https://stackoverflow.com/questions/32910096/is-there-a-way-to-auto-generate-a-str-implementation-in-python
+def auto_str(cls):
+    """Automatically implements __str__ for any class."""
+
+    def __str__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s=%s' % item for item in vars(self).items())
+        )
+
+    cls.__str__ = __str__
+    return cls
+
+
 def d2r(x):
     return x / 180 * math.pi
 
