895c7491f731135c72e09c467d49f1b7c298d3f3
KonstantinosRekoumis <62998716+KonstantinosRekoumis@users.noreply.github.com>
2024-01-12 22:59:25 +0200

    fixed small bug in hydrostatics

--- modules/physics/operations.py
+++ modules/physics/operations.py
@@ -7,7 +7,7 @@ def hydrostatic_pressure(z: float, Zmax: float, rho: float):
     Convention is that the zero is located at the keel plate
     """
     Logger.debug(f"Called with Z:{z} Zmax:{Zmax} rho:{rho}")
-    assert z <= 0
+    assert z >= 0
 
     dT = Zmax - z
     return rho * G * dT
